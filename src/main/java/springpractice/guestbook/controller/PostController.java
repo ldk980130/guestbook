e22932603b3911c4d8b10edc6ef5059f6d4e63ed
ldk <ldk980130@gmail.com>
2021-07-27 14:47:43 +0900

    feat: 이름이나 내용 비어있으면 에러페이지로 이동

--- src/main/java/springpractice/guestbook/controller/PostController.java
+++ src/main/java/springpractice/guestbook/controller/PostController.java
@@ -3,11 +3,13 @@ package springpractice.guestbook.controller;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import springpractice.guestbook.Post;
 import springpractice.guestbook.PostService;
 
+import javax.validation.Valid;
 import java.util.Collections;
 import java.util.List;
 
@@ -28,14 +30,14 @@ public class PostController {
     }
 
     @PostMapping("/")
-    public String posting(PostForm form, Model model) {
+    public String posting(@Valid PostForm form, BindingResult result) {
+
+        if (result.hasErrors()) {
+            return "errorpage";
+        }
+
         Post post = Post.createPost(form.getName(), form.getContent());
         postService.post(post);
-        List<Post> posts = postService.searchAll();
-        Collections.reverse(posts);
-
-        model.addAttribute("posts", posts);
-        model.addAttribute("form", new PostForm());
         return "redirect:/";
     }
 
