550747115512f9db5f1340490dfd44a1db38802f
ldk <ldk980130@gmail.com>
2021-07-27 14:40:59 +0900

    feat: 이름이나 내용 비어있으면 에러페이지로 이동

--- src/main/java/springpractice/guestbook/controller/PostForm.java
+++ src/main/java/springpractice/guestbook/controller/PostForm.java
@@ -3,10 +3,14 @@ package springpractice.guestbook.controller;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.validation.constraints.NotEmpty;
+
 @Getter @Setter
 public class PostForm {
 
+    @NotEmpty(message = "이름은 필수 입니다.")
     private String name;
 
+    @NotEmpty(message = "내용은 필수 입니다.")
     private String content;
 }
